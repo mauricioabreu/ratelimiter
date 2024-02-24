1c0ff02a067ad3f6ed5f962e3044ac47f1966279
Maurício Antunes <mauricio.abreua@gmail.com>
2024-02-24 10:24:17 -0300

    feat: Start implementing fixed window counter

--- internal/fixedwindowcounter/fixedwindowcounter.go
+++ internal/fixedwindowcounter/fixedwindowcounter.go
@@ -1,5 +1,7 @@
 package fixedwindowcounter
 
+import "time"
+
 type FixedWindowCounter struct {
 	size      int
 	threshold int
@@ -10,4 +12,10 @@ func New(size, threshold int) *FixedWindowCounter {
 		size:      size,
 		threshold: threshold,
 	}
+}
+
+// currWindow returns the current window index
+func (f *FixedWindowCounter) currWindow() int64 {
+	now := time.Now()
+	return (now.Unix() / int64(f.size)) * int64(f.size)
 }
