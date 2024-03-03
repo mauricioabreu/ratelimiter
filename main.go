02ed05e9e811f09b28dfce391d206bc2afbb923a
Maurício Antunes <mauricio.abreua@gmail.com>
2024-03-03 16:39:27 -0300

    feat: Implement fixed window counter middleware

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/mauricioabreu/ratelimiter/internal/fixedwindowcounter"
 	"github.com/mauricioabreu/ratelimiter/internal/tokenbucket"
 )
 
@@ -22,7 +23,19 @@ func TokenBucketMiddleware(tb *tokenbucket.TokenBucket, keyExtractor func(echo.C
 	}
 }
 
-func tbLimitedHandler(c echo.Context) error {
+func FixedWindowCounterMiddleware(fwc *fixedwindowcounter.FixedWindowCounter, keyExtractor func(echo.Context) string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			key := keyExtractor(c)
+			if !fwc.Increment(key) {
+				return c.String(http.StatusTooManyRequests, "Rate limit exceeded!\n")
+			}
+			return next(c)
+		}
+	}
+}
+
+func limitHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "You still have requests to spend!\n")
 }
 
@@ -34,8 +47,11 @@ func main() {
 	tb := tokenbucket.New(bucketCapacity, 1, 60)
 	go tb.Refill()
 
+	fwc := fixedwindowcounter.New(60, 10)
+
 	e := echo.New()
-	e.GET("/tb/limited", tbLimitedHandler, TokenBucketMiddleware(tb, keyExtractor))
+	e.GET("/tb/limited", limitHandler, TokenBucketMiddleware(tb, keyExtractor))
+	e.GET("/ftw/limited", limitHandler, FixedWindowCounterMiddleware(fwc, keyExtractor))
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
